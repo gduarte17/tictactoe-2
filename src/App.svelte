f44e5645a906451a54d165682979c5d82ae9f038
gduarte17 <gduarte17k@gmail.com>
2023-11-06 23:06:08 -0300

    mega tictactoe now is the app.svelte himself

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,71 @@
   import Title from "./Title.svelte";
 
   //   export let name;
+
+  let board = Array(9); //.fill(TicTacToe); // Representa o estado do tabuleiro
+  let currentPlayer = "X"; // Inicia com o jogador X
+  let whereNext = "";
+  let isActive = true;
+  /**
+   * @type {string | null}
+   */
+  let winner = ""; // Armazena o vencedor (X, O, ou null)
+
+  // Função whereNext
+  /**
+   * @param {{ detail: any; }} event
+   */
+  function define_whereNext(event) {
+    whereNext = event.detail;
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+  /**
+   * @param {{toString: () => string;}} elementIndex
+   * @param {string} WN_Value
+   */
+  function define_isActive(WN_Value, elementIndex) {
+    if (WN_Value != "") {
+      elementIndex == WN_Value ? (isActive = true) : (isActive = false);
+      console.log(isActive, WN_Value, elementIndex);
+      return isActive;
+    } else if (WN_Value == "") {
+      isActive = true;
+      console.log(WN_Value);
+      return isActive;
+    }
+  }
+
+  // Função para reiniciar o jogo
+  function resetGame() {
+    board = Array(9).fill("");
+    currentPlayer = "X";
+    winner = "";
+  }
 </script>
 
 <main>
   <Title />
-  <Tictactoe />
+  <div class="mega-game">
+    <div class="mega-tictactoe-grid">
+      {#each board as ticGame, index}
+        <div
+          class="{define_isActive(whereNext, index.toString())
+            ? 'active'
+            : 'inactive'} border-{index}"
+        >
+          <Tictactoe
+            {currentPlayer}
+            on:whereNext_Updated={define_whereNext}
+            megaIndex={index}
+          />
+        </div>
+      {/each}
+    </div>
+    {#if winner}
+      <p>{winner === "Empate" ? "Empate!" : `Vencedor: ${winner}`}</p>
+      <button on:click={resetGame}>Reiniciar</button>
+    {/if}
+  </div>
   <!-- <h1>Hello {name}!</h1>
   <p>
     Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
